63e45e8dd1e86b9d8b0ea83a40072861c04cfd52
Erwan LEBOUCHER <elebouch@e3r12p10.42.fr>
2017-11-20 15:05:03 +0100

    initial work

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elebouch <elebouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/20 14:59:16 by elebouch          #+#    #+#             */
+/*   Updated: 2017/11/20 15:03:34 by elebouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putstr(line);
+		ft_putchar('\n');
+		free(line);
+	}
+	if (line)
+		free(line);
+	if (argc == 2)
+		close(fd);
+}
